1838dee602537f8e33b9ac56e7cf9027f05e1a05
Iaroslava <floweryaroslava@mail.ru>
2024-07-25 02:12:48 +0800

    1

--- src/capitalize.js
+++
@@ -1,9 +0,0 @@
-const capitalize = (text) => {
-  if (text.length === 0) {
-    return text;
-  }
-  const [firstChar, ...restChars] = text;
-  return `${firstChar.toUpperCase()}${restChars.join('')}`;
-  };
-
-  export default capitalize;
